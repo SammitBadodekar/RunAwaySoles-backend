6a3e4e9947b55ef4a9e1e177b5e9f35902a47fca
sammit badodekar <badodekarsammit@gmail.com>
2023-05-29 11:30:26 +0530

    Cors again

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const authentication = require("./routes/authentication");
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: "GET,PUT,POST,DELETE",
     credentials: true,
   })
